64fc5ef296215c29ebbf6d08698a0af76d253d02
Patrick Demian <patrick.demian@ryerson.ca>
2014-04-24 15:33:34 -0400

    Adding in project build/settings
    Adding in project build/settings

---
+++ src/MathEvaluator/MathEvaluator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathEvaluator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<Object> o = Calc.Lexxer("3 * log(16 + 5) - 4 / e + - 3.0");
+
+            foreach (Object s in o)
+                Console.WriteLine(s.GetType() + " " + s);
+
+            Console.Read();
+        }
+    }
+}
