fe7eb71b94f172cfdfe4b334758205493875db08
Patrick Demian <patrick.demian@ryerson.ca>
2014-04-25 16:33:28 -0400

    Fixing stuff
    Renamed a bunch of things, modified the code a bit, and fixed some bugs.
    Next build: clean up code!

--- src/MathEvaluator/Calc.cs
+++ src/MathEvaluator/Calc.cs
@@ -42,9 +42,9 @@ class Calc
         public const string E   = "e";
         public const string PHI = "phi";
 
-        public const double PI_VALUE  = 3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798;
-        public const double E_VALUE   = 2.71828182845904523536028747135266249775724709369995957496696762772407663035354759457138217852516642742;
-        public const double PHI_VALUE = 1.61803398874989484820458683436563811772030917980576286213544862270526046281890244970720720418939113748;
+        public const double PI_VALUE  = 3.141592653589793238;
+        public const double E_VALUE   = 2.718281828459045235;
+        public const double PHI_VALUE = 1.618033988749894848;
     }
 
     //Evaluator evaluates the tokens into a doubleing-point number
@@ -105,8 +105,8 @@ class Calc
                     if (current.Equals(Symbols.ADD)) temp += Stack.Pop();
                     else if (current.Equals(Symbols.SUB)) temp = Stack.Pop() - temp;
                     else if (current.Equals(Symbols.MUL)) temp *= Stack.Pop();
-                    else if(current.Equals(Symbols.DIV)) { if(temp == 0.0) throw new Exception("Division by zero"); else temp = Stack.Pop() / temp; }
-                    else if(current.Equals(Symbols.MOD)) { if(temp == 0.0) throw new Exception("Division by zero"); else temp = Stack.Pop() % temp; }
+                    else if(current.Equals(Symbols.DIV)) { if(temp == 0.0) throw new DivideByZeroException("Division by zero in expression"); else temp = Stack.Pop() / temp; }
+                    else if(current.Equals(Symbols.MOD)) { if(temp == 0.0) throw new DivideByZeroException("Division by zero in expression"); else temp = Stack.Pop() % temp; }
                     else temp = Math.Pow(Stack.Pop(), temp);
 
                     Stack.Push(temp);
@@ -124,16 +124,17 @@ class Calc
     private static Queue<Object> ToRPN(Queue<Object> str)
     {
         Stack<Object> Stack = new Stack<Object>();
-        Queue<Object> Temp_queue = new Queue<Object>();
+        Queue<Object> Queue = new Queue<Object>();
 
         for (int TokenCounter = 0; TokenCounter < str.Count; TokenCounter++)
         {
             //if the token is a number, add it to the output queue.
             #region Numbers
-            if (str.ElementAt(TokenCounter) is double) Temp_queue.Enqueue(str.ElementAt(TokenCounter));
+            if (str.ElementAt(TokenCounter) is double) Queue.Enqueue(str.ElementAt(TokenCounter));
             #endregion
 
             //if the token is a left parenthesis, push it onto the stack
+                //TODO: if there is something like "15(...)", make it "15 * (...)"
             #region Left Paren
             else if (str.ElementAt(TokenCounter).Equals(Symbols.LEFT_PAREN)) Stack.Push(Symbols.LEFT_PAREN);
             #endregion
@@ -151,7 +152,7 @@ class Calc
                         Stack.Pop();
                         IsParenMatched = true;
                     }
-                    else Temp_queue.Enqueue(Stack.Pop());
+                    else Queue.Enqueue(Stack.Pop());
                 }
 
                 //If the stack runs out without finding a left parenthesis, then there are mismatched parentheses.
@@ -171,7 +172,7 @@ class Calc
                         !Stack.Peek().Equals(Symbols.LEFT_PAREN) &&
                         !(Stack.Peek().Equals(Symbols.ADD) || Stack.Peek().Equals(Symbols.SUB)))
                     {
-                        Temp_queue.Enqueue(Stack.Pop());
+                        Queue.Enqueue(Stack.Pop());
                     }
                     
                 }
@@ -182,7 +183,7 @@ class Calc
                     while (Stack.Count > 0 &&
                         !Stack.Peek().Equals(Symbols.LEFT_PAREN))
                     {
-                        Temp_queue.Enqueue(Stack.Pop());
+                        Queue.Enqueue(Stack.Pop());
                     }
                 }
 
@@ -198,46 +199,46 @@ class Calc
         while (Stack.Count > 0)
         {
             if (Stack.Peek().Equals(Symbols.LEFT_PAREN)) throw new Exception("Parentheses mismatch");
-            Temp_queue.Enqueue(Stack.Pop());
+            Queue.Enqueue(Stack.Pop());
         }
 
 
-        return Temp_queue;
+        return Queue;
     }
 
     //Parser tokenizes the input string for evaluating
     private static Queue<Object> Parser(string str)
     {
-        if (str == null || str.Length == 0) throw new Exception("Empty string");
+        if (str == null || str.Length == 0) throw new ArgumentException("Empty string");
 
-        Queue<Object> Temp_queue = new Queue<object>();
-        StringBuilder Temp_string = new StringBuilder(new string(str.Where(x => !Char.IsWhiteSpace(x)).ToArray()));
+        Queue<Object> Queue = new Queue<object>();
+        StringBuilder Stringbuilder = new StringBuilder(new string(str.Where(x => !Char.IsWhiteSpace(x)).ToArray()));
        
 
-        for (int TokenCounter = 0; TokenCounter < Temp_string.Length; TokenCounter++)
+        for (int TokenCounter = 0; TokenCounter < Stringbuilder.Length; TokenCounter++)
         {
             //parse ints/doubles
             #region numbers
-            if (Temp_string[TokenCounter] >= '0' && Temp_string[TokenCounter] <= '9')
+            if (Stringbuilder[TokenCounter] >= '0' && Stringbuilder[TokenCounter] <= '9')
             {
-                string ParseDigits = Temp_string[TokenCounter].ToString();
+                string ParseDigits = Stringbuilder[TokenCounter].ToString();
                 bool HasDecimal = false;
 
                 //there are some errors with digits that have a length of 1 at the end of the string. Note: clean this up
-                if (TokenCounter + 1 < Temp_string.Length)
+                if (TokenCounter + 1 < Stringbuilder.Length)
                 {
-                    for (; Temp_string[TokenCounter+1] == '.' || (Temp_string[TokenCounter+1] >= '0' && Temp_string[TokenCounter+1] <= '9');)
+                    for (; Stringbuilder[TokenCounter+1] == '.' || (Stringbuilder[TokenCounter+1] >= '0' && Stringbuilder[TokenCounter+1] <= '9');)
                     {
                         TokenCounter++;
-                        if (Temp_string[TokenCounter] == '.' && !HasDecimal)
+                        if (Stringbuilder[TokenCounter] == '.' && !HasDecimal)
                         {
                             ParseDigits += '.';
                             HasDecimal = true;
                         }
-                        else if (Temp_string[TokenCounter] == '.' && HasDecimal) throw new Exception("Unmatched decimal point at position '" + TokenCounter + "'");
-                        else ParseDigits += Temp_string[TokenCounter];
+                        else if (Stringbuilder[TokenCounter] == '.' && HasDecimal) throw new Exception("Unmatched decimal point at position '" + TokenCounter + "'");
+                        else ParseDigits += Stringbuilder[TokenCounter];
 
-                        if (TokenCounter + 1 >= Temp_string.Length) break;
+                        if (TokenCounter + 1 >= Stringbuilder.Length) break;
                     }
                 }
 
@@ -248,12 +249,12 @@ class Calc
                 //if we're at the beginning
                 if (TokenCounter == 2)
                 {
-                    if (Temp_queue.ElementAt(Temp_queue.Count - 1).Equals(Symbols.SUB)) negate = true;
+                    if (Queue.ElementAt(Queue.Count - 1).Equals(Symbols.SUB)) negate = true;
                 }
                 //otherwise
                 else if(TokenCounter > 2)
                 {
-                    if (Temp_queue.ElementAt(Temp_queue.Count - 1).Equals(Symbols.SUB) && !(Temp_queue.ElementAt(Temp_queue.Count - 2) is double)) negate = true;
+                    if (Queue.ElementAt(Queue.Count - 1).Equals(Symbols.SUB) && !(Queue.ElementAt(Queue.Count - 2) is double)) negate = true;
                 }
 
                 if (negate)
@@ -261,21 +262,21 @@ class Calc
                     ParseDigits = Symbols.SUB + ParseDigits;
 
                     //because you can only remove the start of a queue, reverse it, dequeue, and reverse that
-                    Temp_queue = new Queue<object>(Temp_queue.Reverse());
-                    Temp_queue.Dequeue();
-                    Temp_queue = new Queue<object>(Temp_queue.Reverse());
+                    Queue = new Queue<object>(Queue.Reverse());
+                    Queue.Dequeue();
+                    Queue = new Queue<object>(Queue.Reverse());
                 }
-                Temp_queue.Enqueue(double.Parse(ParseDigits));
+                Queue.Enqueue(double.Parse(ParseDigits));
             }
             #endregion
             //parse words
             #region words
-            else if (Char.IsLetter(Temp_string[TokenCounter]))
+            else if (Char.IsLetter(Stringbuilder[TokenCounter]))
             {
                 //get the entire word
                 StringBuilder sb = new StringBuilder();
-                while (TokenCounter < Temp_string.Length && Char.IsLetter(Temp_string[TokenCounter]))
-                    sb.Append(Temp_string[TokenCounter++]);
+                while (TokenCounter < Stringbuilder.Length && Char.IsLetter(Stringbuilder[TokenCounter]))
+                    sb.Append(Stringbuilder[TokenCounter++]);
 
                 //temp bug fix. eats up next character if uncommented
                 TokenCounter--;
@@ -298,81 +299,116 @@ class Calc
                     case Functions.CEIL:
                     case Functions.ROUND:
                     case Functions.ABS:
-                        Temp_queue.Enqueue(word);
-                        continue;
+                        Queue.Enqueue(word);
+                        break;
+                    
+                        //constants
+                    case Constants.PI:
+                        Queue.Enqueue(Constants.PI_VALUE);
+                        break;
+                    case Constants.E:
+                        Queue.Enqueue(Constants.E_VALUE);
+                        break;
+                    case Constants.PHI:
+                        Queue.Enqueue(Constants.PHI_VALUE);
+                        break;
+                    default:
+                        throw new Exception("Unknown function or constant: \"" + word + "\"");
                 }
-
-
-                if (word.Equals(Constants.PI) || word.Equals(Constants.E) || word.Equals(Constants.PHI))
-                {
-                    double constant = word.Equals(Constants.PI) ? Constants.PI_VALUE :
-                        word.Equals(Constants.E) ? Constants.E_VALUE :
-                        Constants.PHI_VALUE;
-
-                    //two tokens in a row and the second token being '-' means it a negative number
-                    bool negate = false;
-                    //if we're at the beginning
-                    if (Temp_queue.Count < 2)
-                    {
-                        if (Temp_queue.ElementAt(Temp_queue.Count - 1).Equals(Symbols.SUB)) negate = true;
-                    }
-                    //otherwise
-                    else if (TokenCounter > 2)
-                    {
-                        if (Temp_queue.ElementAt(Temp_queue.Count - 1).Equals(Symbols.SUB) && !(Temp_queue.ElementAt(Temp_queue.Count - 2) is double)) negate = true;
-                    }
-
-                    if (negate)
-                    {
-                        //because you can only remove the start of a queue, reverse it, dequeue, and reverse that
-                        Temp_queue = new Queue<object>(Temp_queue.Reverse());
-                        Temp_queue.Dequeue();
-                        Temp_queue = new Queue<object>(Temp_queue.Reverse());
-                        constant = -constant;
-                    }
-                    Temp_queue.Enqueue(constant);
-                }
-                else throw new Exception("Unknown function or constant: \"" + word + "\"");
             }
             #endregion
             //parse symbols
             #region symbols
             else
             {
-                switch (Temp_string[TokenCounter])
+                switch (Stringbuilder[TokenCounter])
                 {
                     case Symbols.LEFT_PAREN:
-                        Temp_queue.Enqueue(Symbols.LEFT_PAREN);
+                        Queue.Enqueue(Symbols.LEFT_PAREN);
                         continue;
                     case Symbols.RIGHT_PAREN:
-                        Temp_queue.Enqueue(Symbols.RIGHT_PAREN);
+                        Queue.Enqueue(Symbols.RIGHT_PAREN);
                         continue;
                     case Symbols.MUL:
-                        Temp_queue.Enqueue(Symbols.MUL);
+                        Queue.Enqueue(Symbols.MUL);
                         continue;
                     case Symbols.EXP:
-                        Temp_queue.Enqueue(Symbols.EXP);
+                        Queue.Enqueue(Symbols.EXP);
                         continue;
                     case Symbols.DIV:
-                        Temp_queue.Enqueue(Symbols.DIV);
+                        Queue.Enqueue(Symbols.DIV);
                         continue;
                     case Symbols.MOD:
-                        Temp_queue.Enqueue(Symbols.MOD);
+                        Queue.Enqueue(Symbols.MOD);
                         continue;
                     case Symbols.ADD:
-                        Temp_queue.Enqueue(Symbols.ADD);
+                        Queue.Enqueue(Symbols.ADD);
                         continue;
                     case Symbols.SUB:
-                        Temp_queue.Enqueue(Symbols.SUB);
+                        Queue.Enqueue(Symbols.SUB);
                         continue;
                 }
-                throw new Exception("Unknown symbol: '" + Temp_string[TokenCounter] + "'");
+                throw new Exception("Unknown symbol: '" + Stringbuilder[TokenCounter] + "'");
             }
             #endregion
         }
 
 
-        return Temp_queue;
+        //fix negatives, if any
+        for (int i = 1; i < Queue.Count; i++)
+        {
+            //two tokens in a row and the second token being '-' means it a negative number
+            bool negate = false;
+            //if we're at the beginning
+            if (i == 1)
+            {
+                if (Queue.ElementAt(i - 1).Equals(Symbols.SUB)) negate = true;
+            }
+            //otherwise
+            else if (i > 2)
+            {
+                if (Queue.ElementAt(i - 1).Equals(Symbols.SUB) && !(Queue.ElementAt(i - 2) is double)) negate = true;
+            }
+
+
+            if (negate)
+            {
+                if (Queue.ElementAt(i) is double)
+                {
+                    //negate the double
+                    List<object> list = new List<object>(Queue);
+                    list[i] = -(double)list[i];
+                    list.RemoveAt(i - 1);
+                    Queue = new Queue<object>(list);
+                }
+                else
+                {
+                    //place parentheses around expression and multiply by -1
+                    int iterator;
+                    int paren_count = 0;
+                    for (iterator = i; iterator < Queue.Count && paren_count >= 0; iterator++)
+                    {
+                        if (Queue.ElementAt(i).Equals(Symbols.LEFT_PAREN)) paren_count++;
+                        else if (Queue.ElementAt(i).Equals(Symbols.RIGHT_PAREN)) paren_count--;
+                    }
+
+                    List<object> list = new List<object>(Queue);
+
+                    //insert (-1 * ( ... ) ) to avoid any ambiguity
+                    list.RemoveAt(i-1); //remove '-'
+                    list.Insert(i-1, Symbols.LEFT_PAREN);
+                    list.Insert(i, -1.0);
+                    list.Insert(i+1, Symbols.MUL);
+                    list.Insert(i+2, Symbols.LEFT_PAREN);
+                    list.Insert(iterator+3, Symbols.RIGHT_PAREN);
+                    list.Insert(iterator+4, Symbols.RIGHT_PAREN);
+
+                    Queue = new Queue<object>(list);
+                }
+            }
+        }
+
+        return Queue;
     }
 
     public static double Calculate(string str)
